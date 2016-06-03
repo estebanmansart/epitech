1aaad760bc7ee759b4ce69690cbbc06d8f61abe0
estebanmansart <esteban.mansart@epitech.eu>
2016-06-03 17:03:36 +0200

    5 premiers exos
    my_aff_alpha.c
    my_aff_revalpha.c
    my_aff_chiffre.c
    my_isneg.c
    my_aff_comb.c
    m

---
+++ Piscine/Piscine_J_03/my_aff_chiffre.c
@@ -0,0 +1,14 @@
+/*
+** my_aff_chiffre.c for my_aff_chiffre in /home/mansar_e/Perso/Projets/epitech-projects/Piscine/Piscine_J_03
+** 
+** Made by Mansart Esteban
+** Login   <mansar_e@epitech.net>
+** 
+** Started on  Fri Jun  3 16:38:08 2016 Mansart Esteban
+** Last update Fri Jun  3 16:38:38 2016 Mansart Esteban
+*/
+
+int	my_aff_chiffre()
+{
+  return (write(1, "0123456789", 10));
+}
